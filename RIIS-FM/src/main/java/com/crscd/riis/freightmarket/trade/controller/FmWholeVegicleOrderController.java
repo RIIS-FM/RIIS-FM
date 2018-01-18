e98c900e1db124c25e73d55d8202a756558e7fb8
wwj913378119 <913378119@qq.com>
2018-01-18 14:33:15 +0800

    Revert "Revert "2018.1.18 qt""
    This reverts commit ae27d7ee58660aaed41d88f53b356f4a9f3946b8.

--- RIIS-FM/src/main/java/com/crscd/riis/freightmarket/trade/controller/FmWholeVegicleOrderController.java
+++ RIIS-FM/src/main/java/com/crscd/riis/freightmarket/trade/controller/FmWholeVegicleOrderController.java
@@ -1,6 +1,8 @@
 package com.crscd.riis.freightmarket.trade.controller;
 
 
+import java.util.List;
+
 import javax.annotation.Resource;
 
 import org.springframework.stereotype.Controller;
@@ -8,8 +10,11 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.crscd.riis.freightmarket.trade.dto.findOrderDTO;
 import com.crscd.riis.freightmarket.trade.entity.FmTradeOrderInfoBaseEntity;
 import com.crscd.riis.freightmarket.trade.entity.FmTradeOrderInfoWholeVegicleFreightEntity;
+import com.crscd.riis.freightmarket.trade.page.PageModel;
+import com.crscd.riis.freightmarket.trade.service.IFmBaseOrderService;
 import com.crscd.riis.freightmarket.trade.service.IFmTradeOrderInfoBaseService;
 import com.crscd.riis.freightmarket.trade.service.IFmWholeVegicleOrderService;
 
@@ -20,11 +25,13 @@ public class FmWholeVegicleOrderController {
 	private IFmWholeVegicleOrderService fmWholeVegicleOrderService;
 	@Resource
 	private IFmTradeOrderInfoBaseService tradeOrderInfoBaseService;
+	@Resource
+	private IFmBaseOrderService fmBaseOrderService;
 	
 	/* URL：http://localhost:8080/RIIS-FM/fmWholeVegicle/insertWholeVegicleOrder
 	 * 功能：新增整车大宗货物运输需求
-	 * 输入：FmTradeOrderInfoBaseEntity 基本订单信息
-	 * 输出：保存或者提交成功标识符
+	 * @param：FmTradeOrderInfoBaseEntity 基本订单信息
+	 * @return：保存或者提交成功标识符
 	 */
 	@RequestMapping(value="/insertWholeVegicleOrder")
 	@ResponseBody
@@ -41,72 +48,57 @@ public class FmWholeVegicleOrderController {
 				return "defeat";
 		}
 		else{
-			fmWholeVegicleOrderService.saveBasicOrder(record);
-			FmTradeOrderInfoBaseEntity wholeOrder=fmWholeVegicleOrderService.getOrderId(record.getcOrderCode());
+			fmBaseOrderService.saveBasicOrder(record);
+			FmTradeOrderInfoBaseEntity wholeOrder=fmBaseOrderService.getOrderId(record.getcOrderCode());
 			int orderId=wholeOrder.getId();
 			recordWhole.setiOrderId(orderId);
 			fmWholeVegicleOrderService.saveFmWholeVegicleOrder(recordWhole);
 		}
 		return "success";
 		
-		
-	}
-	
-	/* URL：http://localhost:8080/RIIS-FM/fmWholeVegicle/commitWholeVegicleOrder
-	 * 功能：提交整车大宗货物运输需求
-	 * 输入：id
-	 * 输出：提交成功标识符
-	 */
-	@RequestMapping(value="/commitWholeVegicleOrder")
-	@ResponseBody
-	public String commitWholeVegicleOrder(@RequestBody int id) {
-		FmTradeOrderInfoBaseEntity record=fmWholeVegicleOrderService.getBaseOrder(id);
-		int c=fmWholeVegicleOrderService.commitOrder(record);
-		if(c==1)
-			return "提交成功";
-		else return "提交失败";
-		
-	}
-	
-	/* URL：http://localhost:8080/RIIS-FM/fmWholeVegicle/deleteWholeVegicleOrder
-	 * 功能：删除整车大宗货物运输需求
-	 * 输入：id
-	 * 输出：删除成功标识符
-	 */
-	@RequestMapping(value="/deleteWholeVegicleOrder")
-	@ResponseBody
-	public String deleteWholeVegicleOrder(@RequestBody int id) {
-		FmTradeOrderInfoBaseEntity record=fmWholeVegicleOrderService.getBaseOrder(id);
-		int c=fmWholeVegicleOrderService.commitOrder(record);
-		if(c==1)
-			return "删除成功";
-		else return "删除失败";
 	}
 	
 	/* URL：http://localhost:8080/RIIS-FM/fmWholeVegicle/getOrderInfo
 	 * 功能：查看整车大宗货物运输需求
-	 * 输入：FmTradeOrderInfoBaseEntity 基本订单信息
-	 * 输出：整车大宗货物运输需求信息
+	 * @param：FmTradeOrderInfoBaseEntity 基本订单信息
+	 * @return：整车大宗货物运输需求信息
 	 */
 	@RequestMapping(value="/getOrderInfo")
 	@ResponseBody
 	public FmTradeOrderInfoBaseEntity getOrderInfo(@RequestBody FmTradeOrderInfoBaseEntity record) {
 		int id=record.getId();
-		FmTradeOrderInfoBaseEntity orderInfoBase=fmWholeVegicleOrderService.getBaseOrder(id);
+		FmTradeOrderInfoBaseEntity orderInfoBase=fmBaseOrderService.getBaseOrder(id);
 		orderInfoBase.setFmTradeOrderInfoWholeVegicleFreightRecord(fmWholeVegicleOrderService.getFmWholeVegicleOrder(id).get(0));	
 		return orderInfoBase;
 	}
 	
 	/* URL：http://localhost:8080/RIIS-FM/fmWholeVegicle/modifyWholeVegicleOrder
 	 * 功能：修改整车大宗货物运输需求
-	 * 输入：FmTradeOrderInfoBaseEntity 基本订单信息
-	 * 输出：修改大宗货物运输需求信息
+	 * @param：FmTradeOrderInfoBaseEntity 基本订单信息
+	 * @return：修改大宗货物运输需求信息
 	 */
 	@RequestMapping(value="/modifyWholeVegicleOrder")
 	@ResponseBody
 	public String modifyWholeVegicleOrder(@RequestBody FmTradeOrderInfoBaseEntity record) {
-		fmWholeVegicleOrderService.modifyOrder(record);
+		fmBaseOrderService.modifyBasicOrder(record);
 		return "修改成功";
+	}
+	
+    /* URL：http://localhost:8080/RIIS-FM/fmWholeVegicle/findorder
+     * 功能：通过iSenderId查询全部订单信息，并分页显示
+     * @param：dto包括用户id，pageIndex
+     * @return：当前页面的订单信息
+     * */
+	@RequestMapping(value="/findorder")
+	@ResponseBody
+	public List<FmTradeOrderInfoBaseEntity> findorder(@RequestBody findOrderDTO dto){
+		/*PageModel pageModel=new PageModel();
+		return fmWholeVegicleOrderService.findOrder(iAccountId, pageModel);*/
+		/*System.out.println(dto.getId());
+		System.out.println(dto.getPageModel().getPageIndex());*/
+		PageModel pageModel=dto.getPageModel();
+		int iSenderId=dto.getId();
+		return fmBaseOrderService.findOrder(iSenderId, pageModel);
 	}
 
 }
--- RIIS-FM/src/main/java/com/crscd/riis/freightmarket/trade/service/IFmWholeVegicleOrderService.java
+++ RIIS-FM/src/main/java/com/crscd/riis/freightmarket/trade/service/IFmWholeVegicleOrderService.java
@@ -1,24 +1,12 @@
 package com.crscd.riis.freightmarket.trade.service;
 
-import java.util.Date;
 import java.util.List;
 
-import com.crscd.riis.freightmarket.trade.entity.FmTradeOrderInfoBaseEntity;
 import com.crscd.riis.freightmarket.trade.entity.FmTradeOrderInfoWholeVegicleFreightEntity;
-
 
 public interface IFmWholeVegicleOrderService {
 	public int saveFmWholeVegicleOrder(FmTradeOrderInfoWholeVegicleFreightEntity record); //保存整车运输需求
-	public int saveBasicOrder(FmTradeOrderInfoBaseEntity record);//保存基本订单需求
-	public FmTradeOrderInfoBaseEntity getOrderId(String cOrderCode);//获得订单id
-//	public FmTradeOrderTypeEntity selectOrderType(String cTypeValue);//选择订单类型
-	public int commitOrder(FmTradeOrderInfoBaseEntity record);//提交订单
-	public int deleteOrder(FmTradeOrderInfoBaseEntity record);//删除订单
-	public int modifyOrder(FmTradeOrderInfoBaseEntity record);//修改订单
-	public FmTradeOrderInfoBaseEntity getBaseOrder(int id);//通过id获得基本订单需求
 	public List<FmTradeOrderInfoWholeVegicleFreightEntity> getFmWholeVegicleOrder(int iOrderId);//通过订单id获得整车运输需求
-	public String createOrderCode(Date day);//编码生成订单编号
-	public long countOrderNumber(FmTradeOrderInfoWholeVegicleFreightEntity record);//获取订单个数
-	public void showOrdersByPage(Integer iSenderId);//通过托运人id获得该托运人全部订单信息分页显示
+	public long countOrderNumber(FmTradeOrderInfoWholeVegicleFreightEntity record);//获取整车订单个数
 
 }
