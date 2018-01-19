34f507a9ed2f536372380c3a41280404eee1e97f
yuyang2999 <35017809+yuyang2999@users.noreply.github.com>
2018-01-19 15:21:50 +0800

    YY

--- RIIS-FM/src/main/java/com/crscd/riis/freightmarket/trade/tradeConstants/tradeConstants.java
+++ RIIS-FM/src/main/java/com/crscd/riis/freightmarket/trade/tradeConstants/tradeConstants.java
@@ -10,7 +10,7 @@ public class tradeConstants {
 	/** 默认快运的起始标志位为4*/
 	public static int FAST_FREIGHT_FLAG_START = 4;
 	
-	/** 默认快运的终止标志位为4*/
+	/** 默认快运的终止标志位为6*/
 	public static int FAST_FREIGHT_FLAG_END = 6;
 	
 	/** 默认整车运输的起始标志位为7*/
@@ -20,9 +20,9 @@ public class tradeConstants {
 	public static int WHOLE_VEGICLE_FLAG_END = 10;
 	
     /** 默认人工审核的标志位为1*/
-	public static int PEOPLE_AUDIT_TYPE_ID = 1;
+	public static int PEOPLE_AUDIT_TYPE_FLAG = 1;
 	
 	/** 默认系统审核的标志位为0*/
-	public static int SYS_AUDIT_TYPE_ID = 0;
+	public static int SYS_AUDIT_TYPE_FLAG = 0;
 
 }
