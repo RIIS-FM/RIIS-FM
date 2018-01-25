7f78d4fa6c1bf6ca89fbfe75e905ad56254cb212
wwj913378119 <913378119@qq.com>
2018-01-25 11:46:04 +0800

    xiugai

--- RIIS-FM/src/main/java/com/crscd/riis/freightmarket/trade/util/tradeConstants/tradeConstants.java
+++ RIIS-FM/src/main/java/com/crscd/riis/freightmarket/trade/util/tradeConstants/tradeConstants.java
@@ -1,45 +1,3 @@
-<<<<<<< HEAD:RIIS-FM/src/main/java/com/crscd/riis/freightmarket/trade/util/tradeConstants/tradeConstants.java
-package com.crscd.riis.freightmarket.trade.util.tradeConstants;
-
-public class tradeConstants {
-	/** 默认每页4条数据*/		
-	public static int PAGE_DEFAULT_SIZE = 4;
-	
-	/** 默认集装箱运输的标志位为3*/
-	public static int BOX_FREIGHT_FLAG = 3;
-	
-	/** 默认快运的起始标志位为4*/
-	public static int FAST_FREIGHT_FLAG_START = 4;
-	
-	/** 默认快运的终止标志位为6*/
-	public static int FAST_FREIGHT_FLAG_END = 6;
-	
-	/** 默认整车运输的起始标志位为7*/
-	public static int WHOLE_VEGICLE_FLAG_START = 7;
-	
-	/** 默认整车运输的终止标志位为10*/
-	public static int WHOLE_VEGICLE_FLAG_END = 10;
-	
-    /** 默认人工审核的标志位为1*/
-	public static int PEOPLE_AUDIT_TYPE_FLAG = 1;
-	
-	/** 默认系统审核的标志位为0*/
-	public static int SYS_AUDIT_TYPE_FLAG = 0;
-
-	/** 20英寸集装箱*/
-	public static int BOX_TYPE_20INCH = 0;
-	
-	/** 40英寸集装箱*/
-	public static int BOX_TYPE_40INCH = 1;
-	
-	/** 运单未支付标志为0*/
-	public static int WAYBILL_STATE_NON_PAYMENT = 0;
-	
-	/** 运单已支付标志为1*/
-	public static int WAYBILL_STATE_PAYMENT = 1;
-
-}
-=======
 package com.crscd.riis.freightmarket.trade.util.tradeConstants;
 
 public class tradeConstants {
@@ -98,4 +56,3 @@ public class tradeConstants {
 	public static int AUDIT_PASS_FLAG = 1;
 
 }
->>>>>>> 4beca1a88c27f2eaadfe4360e22b0cfdc5fe97a8:RIIS-FM/src/main/java/com/crscd/riis/freightmarket/trade/util/tradeConstants/tradeConstants.java
